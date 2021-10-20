b984f773288216c293647f91d189b86b68435943
Paolo Lami <paololami2000@gmail.com>
2021-10-20 20:22:45 +0000

    dc

--- dcmotor.py
+++ dcmotor.py
@@ -6,14 +6,14 @@ pwmPin = 25
 GPIO.setup(pwmPin, GPIO.OUT)
 
 pwm = GPIO.PWM(pwmPin, 50) # PWM object at 50 Hz (20 ms period)
-pwm.start(1)
+pwm.start(0)
 
 try:
   while True:
     for dc in reversed(range(100)):
       pwm.ChangeDutyCycle(dc)
       print(dc)
-      time.sleep(0.5)
+      time.sleep(0.01)
 except KeyboardInterrupt:
   print("bye")
 GPIO.cleanup() 
